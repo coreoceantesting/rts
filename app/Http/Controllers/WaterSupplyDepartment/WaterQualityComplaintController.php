20500ed17d8a932f6acfdaa1d74ef6cd69e549f8
coreoceantesting <coreoceantesting@gmail.com>
2024-06-18 17:29:34 +0530

    Water Quality Complaint

--- app/Http/Controllers/WaterSupplyDepartment/WaterQualityComplaintController.php
+++ app/Http/Controllers/WaterSupplyDepartment/WaterQualityComplaintController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers\WaterSupplyDepartment;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\WaterDepartment\WaterQuality\CreateRequest;
+use App\Http\Requests\WaterDepartment\WaterQuality\UpdateRequest;
+use App\Services\WaterDepartment\WaterQuality\WaterQualityService;
+use App\Models\WaterDepartment\WaterQualityComplaint;
 
 class WaterQualityComplaintController extends Controller
 {
+    protected $WaterQualityService;
+
+    public function __construct(WaterQualityService $WaterQualityService)
+    {
+        $this->WaterQualityService = $WaterQualityService;
+    }
     /**
      * Display a listing of the resource.
      */
@@ -26,9 +36,19 @@ class WaterQualityComplaintController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateRequest $request)
     {
-        //
+        $WaterQualityService = $this->WaterQualityService->store($request);
+
+        if ($WaterQualityService) {
+            return response()->json([
+                'success' => 'Detail Stored successfully'
+            ]);
+        } else {
+            return response()->json([
+                'error' => 'Something went wrong, please try again'
+            ]);
+        }
     }
 
     /**
@@ -44,7 +64,8 @@ class WaterQualityComplaintController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $data = WaterQualityComplaint::findOrFail($id);
+        return view('WaterSupplyDepartment.WaterQualityComplaints.edit', compact('data'));
     }
 
     /**
@@ -52,7 +73,17 @@ class WaterQualityComplaintController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $WaterQualityService = $this->WaterQualityService->update($request, $id);
+
+        if ($WaterQualityService) {
+            return response()->json([
+                'success' => 'Detail updated successfully'
+            ]);
+        } else {
+            return response()->json([
+                'error' => 'Something went wrong, please try again'
+            ]);
+        }
     }
 
     /**
---
+++ app/Models/WaterDepartment/WaterQualityComplaint.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\WaterDepartment;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WaterQualityComplaint extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'is_aapale_sarkar_payment_paid',
+        'property_owner_name',
+        'aadhar_no',
+        'mobile_no',
+        'email_id',
+        'zone',
+        'ward_area',
+        'plot_no',
+        'house_no',
+        'landmark',
+        'address',
+        'current_connection_is_illegal',
+        'applicant_is_on_rent',
+        'criminal_judicial_issue',
+        'tap_size',
+        'current_existing_tap_type',
+        'place_belongs_to_municipal',
+        'comment',
+        'application_document',
+    ];
+}
