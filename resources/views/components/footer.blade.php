69dd3a2ca1945de4141ff3e3c9757dcc7ca5f049
Core Ocean <coreoceantesting@gmail.com>
2024-06-19 17:26:33 +0530

    fix some changes in footer and dashbboard

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,15 +1,15 @@
 <footer class="footer border-top">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-6 text-dark">
                 <script>
                     document.write(new Date().getFullYear());
                 </script>
                 © {{ config('app.name') }}.
             </div>
             <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    Handcrafted & made with <i class="fas fa-heart text-danger"></i>
+                <div class="text-sm-end d-none d-sm-block text-dark">
+                    Content Own By Panvel Municipal Corporation.
                 </div>
             </div>
         </div>
