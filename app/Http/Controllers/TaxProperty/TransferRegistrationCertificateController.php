b50a3ba0a9c061d64dad17767755ae875b3c3214
coreoceantesting <coreoceantesting@gmail.com>
2024-07-19 16:49:52 +0530

    work on property tax

---
+++ app/Http/Controllers/TaxProperty/TransferRegistrationCertificateController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\TaxProperty;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\PropertyTax\PropertyTaxAssessmentRequest;
+use App\Services\PropertyTax\TransferRegistrationCertificateService;
+
+class TransferRegistrationCertificateController extends Controller
+{
+    protected $transferRegistrationCertificateService;
+
+    public function __construct(TransferRegistrationCertificateService $transferRegistrationCertificateService)
+    {
+        $this->transferRegistrationCertificateService = $transferRegistrationCertificateService;
+    }
+
+    public function create(){
+        return view('PropertyTax.transferRegistrationCertificate.create');
+    }
+
+    public function store(PropertyTaxAssessmentRequest $request){
+        $transferRegistrationCertificate = $this->transferRegistrationCertificateService->store($request);
+
+        if ($transferRegistrationCertificate) {
+            return response()->json([
+                'success' => 'Property registration certificate created successfully'
+            ]);
+        } else {
+            return response()->json([
+                'error' => 'Something went wrong, please try again'
+            ]);
+        }
+    }
+
+    public function edit($id)
+    {
+        $transferRegistrationCertificate = $this->transferRegistrationCertificateService->edit(decrypt($id));
+
+        return view('PropertyTax.transferRegistrationCertificate.edit')->with([
+            'transferRegistrationCertificate' => $transferRegistrationCertificate
+        ]);
+    }
+
+    public function update(PropertyTaxAssessmentRequest $request){
+        $transferRegistrationCertificate = $this->transferRegistrationCertificateService->update($request);
+
+        if ($transferRegistrationCertificate) {
+            return response()->json([
+                'success' => 'Property registration certificate updated successfully'
+            ]);
+        } else {
+            return response()->json([
+                'error' => 'Something went wrong, please try again'
+            ]);
+        }
+    }
+}
---
+++ app/Services/PropertyTax/TransferRegistrationCertificateService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services\PropertyTax;
+
+class TransferRegistrationCertificateService{
+    public function store($request){
+
+    }
+
+    public function edit($id){
+
+    }
+
+    public function update($id){
+
+    }
+}
