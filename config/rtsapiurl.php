bcd1adf5173ad341baf52e416caf3351ee202332
coreoceantesting <coreoceantesting@gmail.com>
2024-09-03 17:05:09 +0530

    Change property tax saptya url

--- config/rtsapiurl.php
+++ config/rtsapiurl.php
@@ -2,7 +2,7 @@
 
 return [
     'marriage' => 'https://pmc-marriage.creatorkart.com/',
-    'propertyTax' => 'http://panvelrtstest.ptaxcollection.com:8080/',
+    'propertyTax' => 'https://panvelrts.ptaxcollection.com/',
     'trade' => 'https://test.tradepanvelmc.org/',
     'water' => 'https://test.paywaterbill.org/',
     'amount' => 23.60,
