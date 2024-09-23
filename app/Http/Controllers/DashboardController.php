e2d8b0155bf9b8f2f8ebb08285de47cbbba551cf
coreoceantesting <coreoceantesting@gmail.com>
2024-09-23 15:28:36 +0530

    fix in changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -38,113 +38,112 @@ class DashboardController extends Controller
     public function myApplication(Request $request)
     {
 
-        if (Auth::user()->is_aapale_sarkar_user) {
-            if (isset($request->str) && $request->str != "") {
-                $str = $request->str;
 
-                $strKeys = "@pn@PNM@m@h@0nl!ne@23523";
-                $strIVs = "PNM@05@3";
+        if (isset($request->str) && $request->str != "") {
+            $str = $request->str;
 
-                // decrypt data and get the reponse data from aapale sarkar
-                $check = $this->aapaleSarkarLoginCheckService->decryptTripleDES($str, $strKeys, $strIVs);
+            $strKeys = "@pn@PNM@m@h@0nl!ne@23523";
+            $strIVs = "PNM@05@3";
 
-                $rowData = explode('|', $check);
+            // decrypt data and get the reponse data from aapale sarkar
+            $check = $this->aapaleSarkarLoginCheckService->decryptTripleDES($str, $strKeys, $strIVs);
 
-                if (count($rowData) > 0) {
-                    if (count($rowData) == 10) {
-                        if ($rowData[8] == "True") {
-                            AapaleSarkarPaymentDetails::create([
-                                'client_code' => $rowData[0],
-                                'service_id' => $rowData[1],
-                                'application_no' => $rowData[2],
-                                'payment_transaction_id' => $rowData[3],
-                                'bank_ref_id' => $rowData[4],
-                                'bank_ref_no' => $rowData[5],
-                                'bank_id' => $rowData[6],
-                                'payment_date' => $rowData[7],
-                                'payment_status' => $rowData[8],
-                                'total_amount' => $rowData[9],
-                            ]);
+            $rowData = explode('|', $check);
 
-                            $serviceDeptId = ServiceCredential::where('service_id', $rowData[1])->value('dept_service_id');
-                            $model = ServiceName::where('service_id', $serviceDeptId)->value('model');
+            if (count($rowData) > 0) {
+                if (count($rowData) == 10) {
+                    if ($rowData[8] == "True") {
+                        AapaleSarkarPaymentDetails::create([
+                            'client_code' => $rowData[0],
+                            'service_id' => $rowData[1],
+                            'application_no' => $rowData[2],
+                            'payment_transaction_id' => $rowData[3],
+                            'bank_ref_id' => $rowData[4],
+                            'bank_ref_no' => $rowData[5],
+                            'bank_id' => $rowData[6],
+                            'payment_date' => $rowData[7],
+                            'payment_status' => $rowData[8],
+                            'total_amount' => $rowData[9],
+                        ]);
 
-                            $model::where('application_no', $rowData[2])->update([
-                                'is_payment_paid_aapale_sarkar' => 1,
-                                'aapale_sarkar_payment_date' => date('Y-m-d')
-                            ]);
+                        $serviceDeptId = ServiceCredential::where('service_id', $rowData[1])->value('dept_service_id');
+                        $model = ServiceName::where('service_id', $serviceDeptId)->value('model');
+
+                        $model::where('application_no', $rowData[2])->update([
+                            'is_payment_paid_aapale_sarkar' => 1,
+                            'aapale_sarkar_payment_date' => date('Y-m-d')
+                        ]);
 
 
-                            $aapaleSarkarCredential = ServiceCredential::where('dept_service_id', $serviceDeptId)->first();
-                            $serviceDay = ($aapaleSarkarCredential->service_day) ? $aapaleSarkarCredential->service_day : 20;
+                        $aapaleSarkarCredential = ServiceCredential::where('dept_service_id', $serviceDeptId)->first();
+                        $serviceDay = ($aapaleSarkarCredential->service_day) ? $aapaleSarkarCredential->service_day : 20;
 
-                            $send = $this->aapaleSarkarLoginCheckService->encryptAndSendRequestToAapaleSarkar(Auth::user()->trackid, $aapaleSarkarCredential->client_code, Auth::user()->user_id, $aapaleSarkarCredential->service_id, $rowData[2], 'N', 'NA', 'N', 'NA', $serviceDay, date('Y-m-d', strtotime("+$serviceDay days")), config('rtsapiurl.amount'), config('rtsapiurl.requestFlag'), 3, "Under Scrutiny", $aapaleSarkarCredential->ulb_id, $aapaleSarkarCredential->ulb_district, 'NA', 'NA', 'NA', $aapaleSarkarCredential->check_sum_key, $aapaleSarkarCredential->str_key, $aapaleSarkarCredential->str_iv, $aapaleSarkarCredential->soap_end_point_url, $aapaleSarkarCredential->soap_action_app_status_url);
+                        $send = $this->aapaleSarkarLoginCheckService->encryptAndSendRequestToAapaleSarkar(Auth::user()->trackid, $aapaleSarkarCredential->client_code, Auth::user()->user_id, $aapaleSarkarCredential->service_id, $rowData[2], 'N', 'NA', 'N', 'NA', $serviceDay, date('Y-m-d', strtotime("+$serviceDay days")), config('rtsapiurl.amount'), config('rtsapiurl.requestFlag'), 3, "Under Scrutiny", $aapaleSarkarCredential->ulb_id, $aapaleSarkarCredential->ulb_district, 'NA', 'NA', 'NA', $aapaleSarkarCredential->check_sum_key, $aapaleSarkarCredential->str_key, $aapaleSarkarCredential->str_iv, $aapaleSarkarCredential->soap_end_point_url, $aapaleSarkarCredential->soap_action_app_status_url);
 
-                            if ($send) {
-                                return redirect()->route('my-application');
-                            } else {
-                                \Log::error('Payment verification failed for Aapale Sarkar');
-                            }
+                        if ($send) {
+                            return redirect()->route('my-application');
+                        } else {
+                            \Log::error('Payment verification failed for Aapale Sarkar');
                         }
-                    } elseif (isset($request->Appid) && $request->Appid != "") {
-                        $serviceCredential = DB::table('service_credentials')->where([
-                            'ulb_id' => $request->ULBID,
-                            'ulb_district' => $request->ULBDistrict,
-                            'service_id' => $request->ns
-                        ])->first();
+                    }
+                } elseif (isset($request->Appid) && $request->Appid != "") {
+                    $serviceCredential = DB::table('service_credentials')->where([
+                        'ulb_id' => $request->ULBID,
+                        'ulb_district' => $request->ULBDistrict,
+                        'service_id' => $request->ns
+                    ])->first();
 
-                        // return redirect($serviceCredential->service_url);
+                    // return redirect($serviceCredential->service_url);
 
-                        // decrypt data and get the reponse data from aapale sarkar
+                    // decrypt data and get the reponse data from aapale sarkar
 
-                        $check = $this->aapaleSarkarLoginCheckService->checkDecryptData(
-                            $serviceCredential->soap_end_point_url,
-                            $serviceCredential->soap_action_url,
-                            $serviceCredential->check_sum_key,
-                            $serviceCredential->client_code,
-                            $request->str,
-                            $serviceCredential->str_key,
-                            $serviceCredential->str_iv
-                        );
+                    $check = $this->aapaleSarkarLoginCheckService->checkDecryptData(
+                        $serviceCredential->soap_end_point_url,
+                        $serviceCredential->soap_action_url,
+                        $serviceCredential->check_sum_key,
+                        $serviceCredential->client_code,
+                        $request->str,
+                        $serviceCredential->str_key,
+                        $serviceCredential->str_iv
+                    );
 
-                        // condition for logic if we get success
-                        if ($check[0]) {
-                            $data = $check[1];
-                            // return $data;
-                            if (isset($data['UserID']) && !empty($data['UserID'])) {
-                                // Query the database to check if the user exists
-                                $existingUser = User::where('user_id', $data['UserID'])->first();
+                    // condition for logic if we get success
+                    if ($check[0]) {
+                        $data = $check[1];
+                        // return $data;
+                        if (isset($data['UserID']) && !empty($data['UserID'])) {
+                            // Query the database to check if the user exists
+                            $existingUser = User::where('user_id', $data['UserID'])->first();
 
-                                if ($existingUser) {
-                                    Auth::login($existingUser);
+                            if ($existingUser) {
+                                Auth::login($existingUser);
 
-                                    User::where('id', Auth::user()->id)->update(['trackid' => $data['TrackId']]);
-                                } else {
-                                    $user = User::create([
-                                        'name' => ($data['FullName']) ? $data['FullName'] : '',
-                                        'email' => ($data['EmailID']) ? $data['EmailID'] : '',
-                                        'password' => ($data['Password']) ? $data['Password'] : '',
-                                        'mobile' => ($data['MobileNo']) ? $data['MobileNo'] : '',
-                                        'age' => ($data['Age']) ? $data['Age'] : '',
-                                        'gender' => ($data['Gender']) ? $data['Gender'] : '',
-                                        'user_id' => ($data['UserID']) ? $data['UserID'] : '',
-                                        'trackid' => ($data['TrackId']) ? $data['TrackId'] : '',
-                                        'is_aapale_sarkar_user' => 1,
-                                    ]);
-
-                                    DB::table('model_has_roles')->insert(['role_id' => 2, 'model_type' => 'App\Models\User', 'model_id' => $user->id]);
-
-                                    Auth::login($user);
-                                }
-                                if (Auth::check()) {
-                                    return redirect()->route('my-application');
-                                }
+                                User::where('id', Auth::user()->id)->update(['trackid' => $data['TrackId']]);
                             } else {
-                                abort(500);
+                                $user = User::create([
+                                    'name' => ($data['FullName']) ? $data['FullName'] : '',
+                                    'email' => ($data['EmailID']) ? $data['EmailID'] : '',
+                                    'password' => ($data['Password']) ? $data['Password'] : '',
+                                    'mobile' => ($data['MobileNo']) ? $data['MobileNo'] : '',
+                                    'age' => ($data['Age']) ? $data['Age'] : '',
+                                    'gender' => ($data['Gender']) ? $data['Gender'] : '',
+                                    'user_id' => ($data['UserID']) ? $data['UserID'] : '',
+                                    'trackid' => ($data['TrackId']) ? $data['TrackId'] : '',
+                                    'is_aapale_sarkar_user' => 1,
+                                ]);
+
+                                DB::table('model_has_roles')->insert(['role_id' => 2, 'model_type' => 'App\Models\User', 'model_id' => $user->id]);
+
+                                Auth::login($user);
+                            }
+                            if (Auth::check()) {
+                                return redirect()->route('my-application');
                             }
                         } else {
                             abort(500);
                         }
+                    } else {
+                        abort(500);
                     }
                 }
             }
